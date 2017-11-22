3d9983df1ad594c0c348796d0a8cf79d577a0ef3
Cesar <cesar.barboza@protonmail.com>
2017-11-22 14:44:08 -0600

    Automated reports, graphs and stories finished

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,22 +4,39 @@ from . import views
 
 app_name = 'dashboard'
 urlpatterns = [
-    # ex: /polls/
+
     url(r'^$', views.index, name='index'),
-    # first indicator
+
+    url(r'^indicators/UK_economy/$', views.UK_economy, name='UK_economy'),
+
     url(r'^indicators/growth/$', views.growth, name='growth'),
-    # data from first indicator
+
     url(r'^data/growth/$', views.growth_data, name='growth_data'),
-    # second indicator
+
     url(r'^indicators/inflation/$', views.inflation, name='inflation'),
-    # data from second indicator
+
     url(r'^data/inflation/$', views.inflation_data, name='inflation_data'),
 
     url(r'^data/productivity/$', views.productivity, name='productivity'),
 
     url(r'^data/income/$', views.income, name='income'),
-    # third indicator
+
+    url(r'^indicators/confidence/$', views.confidence, name='confidence'),
+
+    url(r'^data/confidence/$', views.confidence_data, name='confidence_data'),
+
+    url(r'^data/business/$', views.business_data, name='business_data'),
+
     url(r'^indicators/trade/$', views.trade, name='trade'),
-    # fourth indicator
+
+    url(r'^data/imports/$', views.imports_data, name='imports_data'),
+
+    url(r'^data/services/$', views.services_data, name='services_data'),
+
     url(r'^indicators/debt/$', views.debt, name='debt'),
+
+    url(r'^data/debt/$', views.debt_data, name='debt_data'),
+
+    url(r'^data/deficit/$', views.deficit_data, name='deficit_data'),
+
     ]
